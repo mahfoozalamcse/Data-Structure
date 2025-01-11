8c864d2956bc04490e80f0df9542df5d0641304c
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-11 09:45:35 +0000

    implement and traverse linkedlist

---
+++ linkedlistimp.java
@@ -0,0 +1,27 @@
+public class linkedlistimp {
+     static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+   //  traverse linkedlist
+   public static void traverse(Node head){
+     Node temp = head;
+     while (temp != null) {
+        System.out.print(temp.data+" ");
+         temp = temp.next;
+     }
+   }
+    public static void main(String[] args) {
+       Node head = new Node(10);
+       head.next = new Node(20);
+       head.next.next = new Node(30);
+   
+      
+
+      traverse(head);
+    }
+}
