a4688903354733b67da2382ec00456357a308e17
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-24 22:19:19 +0530

    print array to check connectivity with github

--- impLL.java
+++ impLL.java
@@ -1,1 +1,12 @@
-// hello
+public class impLL {
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {12, 4, 6};
+        printArr(arr);
+    }
+}
