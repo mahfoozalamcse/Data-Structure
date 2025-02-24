33faa2a1eb4f47b0347e00b0516037a7db0ec27c
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-24 23:34:34 +0530

    start DLL implimentation

---
+++ DLLImp.java
@@ -0,0 +1,15 @@
+public class DLLImp {
+    class DNode{
+        int data;
+        DNode prev, next;
+        DNode(int data){
+            this.data = data;
+            this.prev = this.next = null;
+        }
+    }
+    // insert in DLL linked list
+    
+    public static void main(String[] args) {
+        
+    }
+}
