5157fdb6409bdad03e218a0606b0e897d3da95fe
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-14 19:49:10 +0530

    Update insertAtPos.java

--- insertAtPos.java
+++ insertAtPos.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class insertAtPos {
     static class Node{
         int data;
-        Node next;
+        Node next; 
         Node(int d){
             data = d;
             next = null;
