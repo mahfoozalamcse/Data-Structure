c9e88d9e9f104ab8fa9c94f23532fd9ee0cbe73b
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-25 20:28:33 +0530

    Update linkedlistimp.java

--- linkedlistimp.java
+++ linkedlistimp.java
@@ -1,7 +1,7 @@
 public class linkedlistimp {   
      static class Node{   
         int data;
-        Node next;
+        Node next; 
         Node(int d){ 
             data = d; 
             next = null; 
