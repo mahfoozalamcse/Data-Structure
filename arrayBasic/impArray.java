076fff1fa976073234ba9d79000df4f246f61f37
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-02 17:56:48 +0530

    understand vector in java

--- arrayBasic/impArray.java
+++ arrayBasic/impArray.java
@@ -2,6 +2,7 @@ package arrayBasic;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class impArray {
     public static void main(String[] args) throws IOException {
@@ -14,40 +15,54 @@ public class impArray {
        // size array list
        int n = 5;
 
-       ArrayList<Integer> arrli = new ArrayList<Integer>(n);
-       arrli.add(1);
-       arrli.add(2);
-       arrli.add(3);
-       arrli.add(4);
+        ArrayList<Integer> arrli = new ArrayList<Integer>(n);
+        arrli.add(1);
+    //    arrli.add(2);
+    //    arrli.add(3);
+    //    arrli.add(4);
 
-       for (Integer integer : arrli) {
-             System.out.print(integer+" ");
-       }
+    //    for (Integer integer : arrli) {
+    //          System.out.print(integer+" ");
+    //    }
 
-       System.out.println();
+    //    System.out.println();
 
-       arrli.remove(3);
+    //    arrli.remove(3);
 
-       for (Integer integer : arrli) {
-             System.out.print(integer+" ");
-       }
+    //    for (Integer integer : arrli) {
+    //          System.out.print(integer+" ");
+    //    }
 
-       System.out.println();
+    //    System.out.println();
 
-       for (int i = 0; i < arrli.size(); i++) {
-            System.out.print(arrli.get(i)+" ");
-       }
+    //    for (int i = 0; i < arrli.size(); i++) {
+    //         System.out.print(arrli.get(i)+" ");
+    //    }
 
-       // append new element at the end of list
-       for (int i = 1; i <= n; i++) {
-          arrli.add(i);
-       }
-       
-        System.out.println();
+    //    // append new element at the end of list
+    //    for (int i = 1; i <= n; i++) {
+    //       arrli.add(i);
+    //    }
 
-        for (int i = 0; i < arrli.size(); i++) {
-            System.out.print(arrli.get(i)+" ");
-       }
+    //     System.out.println();
+
+    //     for (int i = 0; i < arrli.size(); i++) {
+    //         System.out.print(arrli.get(i)+" ");
+    //    }
+
+
+
+    // vector in java
+    System.out.println();
+    
+    Vector<Integer> v = new Vector<Integer>(n);
+    v.add(1);
+    v.add(2);
+    v.add(3);
+    v.add(4);
+
+    System.out.print("vector is :" + v);
+
 
     }
 }
