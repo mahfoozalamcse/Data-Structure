e980a25acc1d9d161f5c42dd51c789fadc0c0d6d
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-02 21:01:19 +0530

    merge two sorted array

--- arrayBasic/problemSample.java
+++ arrayBasic/problemSample.java
@@ -53,6 +53,69 @@ public class problemSample {
     }
 
 
+
+    // find largest sum  of positive and negative num
+    public static int largestSum(int arr[]){
+        int maxSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+           int sum = 0;
+            sum += Math.abs(arr[i]);
+            if (sum > maxSum) {
+                maxSum += sum;
+            }
+        }
+
+        return maxSum;
+    }
+
+
+    // merge to sorted array
+    public static void mergeArray(int arr1[], int arr2[], int n, int m){
+        
+        int arr3[]= new int[n+m];
+        
+        int i=0, j=0, k=0;
+
+       while (i < n && j < m) {
+          if (arr1[i] < arr2[j]) {
+              arr3[k] = arr1[i];
+              i++;
+          }else{
+              arr3[k] = arr2[j];
+              j++;
+          }
+
+          k++;
+       }
+
+       // rest of i
+       while (i<n) {
+         arr3[k++] =  arr1[i++];
+       }
+
+       // rest of j
+       while (j<m) {
+         arr3[k++] =  arr2[j++];
+       }
+
+
+       System.out.println("sorted order");
+
+       for (int value : arr3) {
+           System.out.print(value + " ");
+       }
+       System.out.println();
+
+    }
+
+    // print array
+    public static void printArray(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
     public static void main(String[] args) {
         // int arr[] = {2, 3, 5, 7, 9};
         // int n = arr.length;
@@ -71,7 +134,23 @@ public class problemSample {
 
 
 
-        int arr[] = {-7, 1, 5, 2, -4, 3, 0};
-        System.out.println(equlibrium1(arr));
+        // int arr[] = {-7, 1, 5, 2, -4, 3, 0};
+        // System.out.println(equlibrium1(arr));
+
+    //     System.out.println();
+
+    //     int arr[] = {-3, 4, -1, -2, 1, 5};
+    //    System.out.println(largestSum(arr));
+
+
+    int arr1[] = {1, 5, 7, 9};
+    int arr2[] = {2, 6, 8, 11, 13};
+    int n = arr1.length;
+    int m = arr2.length;
+   
+    mergeArray(arr1, arr2, n, m);
+   
+    
+       
     }
 }
