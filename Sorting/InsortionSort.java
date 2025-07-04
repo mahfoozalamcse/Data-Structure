8bbe8dec4ba6114a6d397cc29d70f11a9c4c02c4
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-04 21:25:37 +0530

    practice insertion sort

--- Sorting/InsortionSort.java
+++ Sorting/InsortionSort.java
@@ -17,7 +17,7 @@ public class InsortionSort {
     // print arr
     public static void printArr(int arr[]){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+ " ");
+            System.out.print(arr[i]+ " ");
         }
     }
     public static void main(String[] args) {
