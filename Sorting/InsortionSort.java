08d93d420ca8760f6b9060f808f906a5311fe1c6
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-04 21:24:54 +0530

    practice insertion sort

---
+++ Sorting/InsortionSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class InsortionSort {
+    public static void insertionSort(int arr[]){
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] > key) {
+                  arr[j+1] = arr[j];
+                  j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    // print arr
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+ " ");
+        }
+    }
+    public static void main(String[] args) {
+       int arr[] = {2, 6, 7, 4, 8};
+       insertionSort(arr); 
+       printArr(arr);
+    }
+}
