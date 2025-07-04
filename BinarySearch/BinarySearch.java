7d3e86634cd4d76849f54dfae447e35fd4ce3221
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-04 18:07:53 +0530

    test build binary search and Collections

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,5 +1,10 @@
 package BinarySearch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+//import java.util.List;
+
 public class BinarySearch {
     public static boolean binarysearch(int arr[], int x){
         int start = 0;
@@ -44,8 +49,29 @@ public class BinarySearch {
       int x = 90;
       System.out.println();
       // System.out.println("  "+ binarysearch(arr, x));
-      int start = 0;
-      int end = arr.length-1;
-      System.out.println("  "+ binarysearchrecursive(arr, x, start, end));
+    //   int start = 0;
+    //   int end = arr.length-1;
+    //   System.out.println("  "+ binarysearchrecursive(arr, x, start, end));
+
+    // build in binaary search
+    System.out.println("  "+ Arrays.binarySearch(arr, x));
+    // collection binary serach
+    // work on Array list
+
+    ArrayList<Integer> al = new ArrayList<>();
+       al.add(10);
+       al.add(30);
+       al.add(35);
+       al.add(90);
+       al.add(100);
+
+    int key = 10;
+    int index = Collections.binarySearch(al,key);
+    System.out.println(index);
+
+
+    // search in decending order
+    int index2 = Collections.binarySearch(al, key, Collections.reverseOrder());
+    System.out.println("Found at index : "+index2);
     }
 }
