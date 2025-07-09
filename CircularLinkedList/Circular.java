7a43e47d991f3c211cfc7621270aee9bf4da0096
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-10 00:18:04 +0530

    practice insert begin

--- CircularLinkedList/Circular.java
+++ CircularLinkedList/Circular.java
@@ -25,6 +25,34 @@ public class Circular {
         System.out.println();
     }
 
+    // insertion in circular linked list
+    // insert at begin
+    // insert at end
+    // insert at given index
+
+    public static Node insertBegin(Node head, int k){
+        Node temp = new Node(k);
+
+        if (head == null) {
+            temp.next = temp; // point it self
+            return temp;   // return new head
+        }
+        Node curr = head;
+        while (curr.next != head) {
+            curr = curr.next;
+        }
+
+        // insert at begin
+        curr.next = temp;
+        temp.next = head;
+
+        // return head
+        return temp;
+    } 
+
+    // insert given posion
+    
+
     public static void main(String[] args) {
      Circular cll = new Circular();
      // add in head
@@ -37,6 +65,10 @@ public class Circular {
      second.next = third;
      third.next = cll.head;
 
+     printCLL(cll.head);
+     // insert begin
+     cll.head = insertBegin(cll.head, 5);
+     cll.head = insertBegin(cll.head,10);
      printCLL(cll.head);
 
      
