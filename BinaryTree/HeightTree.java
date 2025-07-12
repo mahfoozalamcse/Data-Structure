c102c7f0dab78e37aa27dfb7544fdfde54134851
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-13 03:49:56 +0530

    practice find height and diametre

--- BinaryTree/HeightTree.java
+++ BinaryTree/HeightTree.java
@@ -102,6 +102,133 @@ public class HeightTree {
            level++;
         }
     }
+
+    // calculate size of tree
+    public static int size(Node root){
+        if (root == null) {
+            return 0;
+        }
+
+        return (size(root.left) + size(root.right))+1;
+    }
+
+    // iterative way calculate size
+    public static int iterativeSize(Node root){
+        if (root == null) {
+            return 0;
+
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        int count = 0;
+
+        while (!q.isEmpty()) {
+             Node curr = q.poll();
+             count++;
+             if (curr.left != null) {
+                q.offer(curr.left);
+             }
+             if (curr.right != null) {
+                q.offer(curr.right);
+             }    
+        }
+        return count;
+    }
+
+    // find maximum in binary tree recursive
+    public static int max(Node root){
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int lMax = max(root.left);
+        int rMax= max(root.right);
+
+        return Math.max(root.data, Math.max(lMax, rMax));
+    }
+
+    // iterative way
+    public static int iterativemax(Node root){
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        int max_Value = Integer.MIN_VALUE;
+
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            max_Value = Math.max(curr.data, max_Value);
+
+            if (curr.left != null) {
+                q.offer(curr.left);
+            }
+            if (curr.right != null) {
+                q.offer(curr.right);
+            }
+        }
+        return max_Value;
+    }
+
+    // find LCA
+    public static Node findLCA(Node root, int n1, int n2){
+        if (root == null) {
+            return null;
+        }
+        if (root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftLCA = findLCA(root.left, n1, n2);
+        Node rightLCA = findLCA(root.right, n1, n2);
+
+        if (leftLCA != null && rightLCA != null) {
+            return root;
+        }
+
+        return (leftLCA != null) ? leftLCA : rightLCA;
+    }
+
+    // Diametre
+    public static int height(Node root){
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(height(root.left), height(root.right))+1;
+    }
+
+    public static int diametre(Node root){
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        int ld = diametre(root.left);
+        int rd = diametre(root.right);
+
+        return Math.max(lh+rh+1, Math.max(ld, rd));
+    }
+
+    // optamize diametre
+    static int maxDiametre = 0;
+    public static int height2(Node root){
+        if (root == null) {
+            return 0;
+        }
+        int lh = height2(root.left);
+        int rh = height2(root.right);
+
+        maxDiametre = Math.max(maxDiametre, lh+rh+1);
+        return 1+Math.max(lh, rh);
+
+
+    }
+
+    public static int getDiametre(Node root){
+        maxDiametre = 0;
+        height2(root);
+        return maxDiametre;
+    }
     public static void main(String[] args) {
         root = new Node(10);
         root.left = new Node(20);
@@ -109,6 +236,9 @@ public class HeightTree {
         root.left.left = new Node(40);
         root.left.right = new Node(50);
         root.right.left = new Node(60);
+        root.right.right = new Node(70);
+        root.left.left.left = new Node(80);
+        root.left.left.right = new Node(90);
 
         System.out.println();
         System.out.println(maxHeight(root));
@@ -123,5 +253,34 @@ public class HeightTree {
         int k = 2;
         System.out.println("Element at : "+ k+ " Level");
         printkDistance2(root, k);
+
+        System.out.println();
+        System.out.println(size(root));
+
+        System.out.println();
+        System.out.println(iterativeSize(root));
+
+        // max
+        System.out.println();
+        System.out.println(max(root));
+
+        System.out.println();
+        System.out.println(iterativemax(root));
+
+        // print LCA
+        System.out.println();
+        Node lca = findLCA(root, 80, 90);
+        System.out.println("LCA of (80, 90) : "+ (lca != null ? lca.data : "Data not found"));
+
+        // diametre
+        System.out.println();
+        System.out.println(diametre(root));
+
+        // optamize diametre 
+        System.out.println();
+        System.out.println(getDiametre(root));
+
+        
+
     }
 }
