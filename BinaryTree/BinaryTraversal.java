8a3c20a8b26caad20883b54b55c3fee275fec7e0
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-12 23:36:30 +0530

    practice level order and insert node in tree

--- BinaryTree/BinaryTraversal.java
+++ BinaryTree/BinaryTraversal.java
@@ -1,5 +1,8 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTraversal {
     public static class Node{
         int data;
@@ -9,7 +12,7 @@ public class BinaryTraversal {
           left = right = null;
         }
     }
-    Node root;
+    static Node root;
     // inorder (left , root, right)
     public static void inorderTraversal(Node node){
         if (node == null) {
@@ -38,24 +41,93 @@ public class BinaryTraversal {
         postOrder(node.right);
         System.out.print(node.data+" ");
     }
+    // level order traversal (BFS)
+    public static void levelOrder(Node node){
+        if (node == null) {
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(node);
+
+        while (!q.isEmpty()) {
+            Node current = q.poll();
+            System.out.print(current.data+" ");
+
+            if (current.left != null) {
+                q.offer(current.left);
+            }
+
+            if (current.right != null) {
+                q.offer(current.right);
+            }
+        }
+
+    }
+
+    // insert a new node in level order
+    public static void insertLevel(int value){
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+ 
+        q.offer(root);
+        while (!q.isEmpty()) {
+           Node current = q.poll();
+
+           // left is empty levele insert
+           if (current.left == null) {
+              current.left = newNode;
+              return;
+           }else{
+              q.offer(current.left);
+
+           }
+
+           // if right level insert
+           if (current.right == null) {
+               current.right = newNode;
+           }else{
+              q.offer(current.right);
+           }
+        }
+    }
+
+    
     public static void main(String[] args) {
       BinaryTraversal tree = new BinaryTraversal();
-      tree.root = new Node(10);
-      tree.root.left = new Node(20);
-      tree.root.right = new Node(30);
-      tree.root.left.left = new Node(40);
-      tree.root.left.right = new Node(50);
+      BinaryTraversal.root = new Node(10);
+      BinaryTraversal.root.left = new Node(20);
+      BinaryTraversal.root.right = new Node(30);
+      BinaryTraversal.root.left.left = new Node(40);
+      BinaryTraversal.root.left.right = new Node(50);
 
       System.out.println();
       System.out.println("Inorder.. ");
-      inorderTraversal(tree.root);
+      inorderTraversal(BinaryTraversal.root);
 
       System.out.println();
       System.out.println("Preorder.. ");
-      preOrder(tree.root);
+      preOrder(BinaryTraversal.root);
 
       System.out.println();
       System.out.println("Postorder..");
-      postOrder(tree.root);
+      postOrder(BinaryTraversal.root);
+
+      System.out.println();
+      System.out.println("Level order traversal.. ");
+      levelOrder(BinaryTraversal.root);
+
+      // inserting element
+      System.out.println();
+      System.out.println("Level insert .. ");
+      BinaryTraversal.insertLevel(60);
+      BinaryTraversal.insertLevel(45);
+      levelOrder(BinaryTraversal.root);
+
     }
 }
---
+++ BinaryTree/DeleteNode.java
@@ -0,0 +1,13 @@
+package BinaryTree;
+
+public class DeleteNode {
+        // To delete a node with a given value:
+             // Find the node to delete (target node).
+             // Find the deepest rightmost node in the tree.
+             // Copy the data of the deepest node to the node-to-delete.
+             // Delete the deepest node.
+    
+    public static void main(String[] args) {
+        
+    }
+}
